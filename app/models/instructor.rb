12a955a2881796da336b180c824393c2a1dab4ba
mmacdonald1 <melmacd94@gmail.com>
2018-06-12 15:46:23 -0400

    Failed and Read corrected

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,8 +13,8 @@ def self.all
   @@all
 end
 
-def fail_student(student_first_name_input)
-  chosen_test = BoatingTest.all.find{|test| test.student.first_name == student_first_name_input}
+def fail_student(student_first_name_input, test_name)
+  chosen_test = BoatingTest.all.find{|test| test.student.first_name == student_first_name_input && test.name == test_name}
   chosen_test.status= "failed"
 end
 
